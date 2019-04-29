c48f3f53449a1cfe16da8c066b8fd7b3d77f548f
Po Rith <poo@Poamrongs-MBP.attlocal.net>
2019-04-29 15:09:32 -0700

    commit

--- pages/artwork/_model.vue
+++ pages/artwork/_model.vue
@@ -3,8 +3,8 @@
     <div class="container">
       <div class="row pt-1 pb-4">
         <div  class="col-md-8 pb-3">
-          <transition name="fade" appear>
-            <img :load="loadHandler" :src="currPortrait.hires" v-show="isLoaded" class="w-100" alt="Portrait Image">
+          <transition name="fade" v-show="isLoaded" appear>
+            <img :load="loadHandler" :src="currPortrait.hires" class="w-100" alt="Portrait Image">
           </transition>
           <div v-show="!isLoaded" class="panel w-100 bg-custom text-center">
             <span class="font-custom">PR</span>
